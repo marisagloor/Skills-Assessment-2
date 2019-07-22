76fb7d100c5a3eb41992d656e4287a4adc7479be
Risa <marisagloor@gmail.com>
2019-07-21 18:32:27 -0700

    skills-2 initial commit

---
+++ oo.py
@@ -0,0 +1,50 @@
+"""
+1. Discussion
+-------------
+
+1. What are the three main design advantages that object orientation
+   can provide? Explain each concept.
+
+2. What is a class?
+
+3. What is a method?
+
+4. What is an instance in object orientation?
+
+5. How is a class attribute different than an instance attribute?
+   Give an example of when you might use each.
+
+"""
+
+
+# 2. Road Class
+# #############################################################################
+
+# Create your Road class here
+
+# Instantiate Road objects here
+
+
+# 3. Update Password
+# #############################################################################
+class User(object):
+    """A user object."""
+
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+
+    # write the update_password method here
+
+
+# 4. Build a Library
+# #############################################################################
+class Book(object):
+    """A Book object."""
+
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+
+# Create your Library class here
+
